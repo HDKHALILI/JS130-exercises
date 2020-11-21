0214ce688ae108a53690ed38d86f7847d3ec0698
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2020-11-21 14:45:35 +1100

    add myBindImproved.js

---
+++ myBindImproved.js
@@ -0,0 +1,19 @@
+// Our earlier implementation of Function.prototype.bind as myBind was
+// simplistic. Function.prototype.bind has another trick up its sleeve
+// besides hard-binding functions to context objects. It's called partial
+// function application.
+
+// Alter the myBind function written in the previous exercise to support
+// partial function application.
+
+// using function declaration
+function myBind(context, func) {
+  let partialArgs = Array.from(arguments).slice(2);
+
+  return function() {
+    let remainingArgs = Array.from(arguments).slice();
+    let allArgs = partialArgs.concat(remainingArgs);
+
+    return func.apply(context, allArgs);
+  }
+}
